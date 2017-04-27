7831a806b3ebf53f48763b2b9dd2a880f15cb9d6
AJ <ajoeford@gmail.com>
2017-04-26 18:07:12 -0700

    Added basic team class, need to move code out of bin/run

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,8 +1,23 @@
 class Scraper
 
   def self.scrape_for_teams(url)
+    doc = Nokogiri::HTML(open(url))
+    teams = doc.css(".teamsList tr td > a:first-child + a")
+    teams
+  end
 
+  def self.scrape_team_schedule(url)
+    #to complete
+  end
 
+  def self.make_teams(url)
+    Scraper.scrape_for_teams(url).each do |team|
+      Team.create(team.text)
+    end
+  end
+
+  def self.make_schedules
+    #to complete
   end
 
 end
---
+++ lib/team.rb
@@ -0,0 +1,23 @@
+class Team
+  attr_accessor :name
+
+  @@all_teams = []
+
+  def self.create(name)
+    unless @@all_teams.detect{|team| team.name == name}
+      new_team = Team.new(name)
+      @@all_teams << new_team
+    end
+  end
+
+  def self.all_teams
+    @@all_teams
+  end
+
+  ### Instance methods ###
+
+  def initialize(name)
+    @name = name
+  end
+
+end
